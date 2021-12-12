5144a6b34137ecfa5813deca82d71ea0c31f1ed3
mahimshridhar <mahimshridhar@gmail.com>
2021-12-12 14:04:48 +0530

    set up styles

---
+++ src/components/Product.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => {
+  return (
+    <p className="products">Please thanks for helping me i hope its all good</p>
+  );
+};
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -44,14 +44,6 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
     ],
   },
 
